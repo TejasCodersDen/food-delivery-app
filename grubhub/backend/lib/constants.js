5f4348dc5b0968c7a71432d51e4d3331a54418cc
tejascu <98671376+tejascu@users.noreply.github.com>
2023-09-25 13:15:56 -0700

    Delete grubhub directory

--- grubhub/backend/lib/constants.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    BUYER : 'buyer',
-    OWNER : 'owner',
-    MONGO_URL : 'mongodb+srv://msadmin:msadmin@test-uolh4.mongodb.net/test?retryWrites=true&w=majority',
-    JWT_KEY : 'GrubHubSecretKEY#421'
-})
